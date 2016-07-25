a662f59202cf355db22345b9caf34545da6b3ec8
Elliot Gorokhovsky <elliot.gorokhovsky@gmail.com>
2016-07-24 21:27:53 -0600

    Changed ordering in the struct for readability

--- eval.h
+++ eval.h
@@ -8,8 +8,8 @@ typedef uint8_t opcode; //for use in the equation tree as function identifiers
 //Define the tree struct
 typedef uint8_t ptr;
 typedef struct node {
-  byte val; //for opcodes const_id and index
   opcode op;
+  byte val; //for opcodes const_id and index
   ptr a;
   ptr b;
 } node;
